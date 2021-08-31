ca17a0b16d2bb8298fef3d2a2c6dfaf198780393
ellackramer <ellackramer@gmail.com>
2021-08-31 19:06:39 -0400

    final push

---
+++ seeds/product-seeds.js
@@ -0,0 +1,38 @@
+const { Product } = require('../models');
+
+const productData = [
+  {
+    product_name: 'T-Shirt',
+    price: 14.99,
+    stock: 14,
+    category_id: 1,
+  },
+  {
+    product_name: 'Sneakers',
+    price: 90.0,
+    stock: 25,
+    category_id: 5,
+  },
+  {
+    product_name: 'Baseball Hat',
+    price: 22.99,
+    stock: 12,
+    category_id: 4,
+  },
+  {
+    product_name: 'CD',
+    price: 12.99,
+    stock: 50,
+    category_id: 3,
+  },
+  {
+    product_name: 'Shorts',
+    price: 29.99,
+    stock: 22,
+    category_id: 2,
+  },
+];
+
+const seedProducts = () => Product.bulkCreate(productData);
+
+module.exports = seedProducts;
